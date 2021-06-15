eff61fbf654111e34e89d6b90baddc9514d43d0e
JoshPenner <ircobra@gmail.com>
2021-06-15 09:15:49 -0700

    Lobby hosting and joining

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject landingPagePanel = null;
+
+    public void HostLobby()
+    {
+        landingPagePanel.SetActive(false);
+
+        NetworkManager.singleton.StartHost();
+    }
+
+
+}
